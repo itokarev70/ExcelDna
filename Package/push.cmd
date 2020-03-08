75f80aafecf27d162290b3bb947a330b7b31d84f
Govert van Drimmelen <govert@icon.co.za>
2020-03-08 23:44:26 +0200

    Update binaries and NuGet batch files

---
+++ Package/push.cmd
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+
+set currentPath=%~dp0
+set basePath=%currentPath:~0,-1%
+set outputPath=%basePath%\nupkg
+
+if not exist "%outputPath%" mkdir "%outputPath%"
+
+nuget.exe push "%outputPath%\Excel-DNA.0.32.1-beta2.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+@if errorlevel 1 goto end
+
+nuget.exe push "%outputPath%\Excel-DNA.Lib.0.32.1-beta2.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+@if errorlevel 1 goto end
+
+:end
